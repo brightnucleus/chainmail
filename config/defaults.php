117f2b93a82862905b6e400d9cb9e4d409b5f44a
Alain Schlesser <alain.schlesser@gmail.com>
2016-01-28 16:07:21 +0100

    Restructure Config file.

--- config/defaults.php
+++ config/defaults.php
@@ -1,6 +1,9 @@
 <?php
 /**
- * ChainMail default configuration settings
+ * ChainMail default configuration settings.
+ *
+ * These defaults can be overridden by providing an instance of a
+ * `ConfigInterface` to the `ChainMail` constructor.
  *
  * @package   brightnucleus/chainmail
  * @author    Alain Schlesser <alain.schlesser@gmail.com>
@@ -10,73 +13,171 @@
  */
 
 return [
-    'views_root'       => __DIR__ . '/../views/',
-    'view_type'        => 'PHPView',
-    'default_template' => 'BasicTemplate',
-    'formats'          => [
+    /*
+     * Root location that contains the view files to be rendered. These are
+     * referenced by the individual sections & templates when choosing a
+     * specific view to be rendered.
+     */
+    'view_root_locations' => [
+        'default' => __DIR__ . '/../views/',
+    ],
+
+    /*
+     * Type of view rendering system to use.
+     */
+    'view_type'           => 'PHPView',
+
+    /*
+     * The default template to when none is specified.
+     */
+    'default_template'    => 'BasicTemplate',
+
+    /*
+     * The formats in which an email can be rendered.
+     * Each format needs a `MailInterface` implementation, a
+     * `ValidatorInterface` implementation, as well as a `SanitizerInterface`
+     * implementation.
+     */
+    'formats'             => [
+
+        /*
+         * The `html` format should be the default format when using responsive
+         * emails.
+         */
         'html' => [
             'mail'      => 'HTMLMail',
             'validator' => 'HTMLValidator',
             'sanitizer' => 'HTMLSanitizer',
         ],
+
+        /*
+         * The `text` format is meant to be used a as a fallback, and can also
+         * be sent as backup content embedded within HTML emails.
+         */
         'text' => [
             'mail'      => 'TextMail',
             'validator' => 'TextValidator',
             'sanitizer' => 'TextSanitizer',
         ],
     ],
-    'mails'            => [
-        'HTMLMail' => '\BrightNucleus\ChainMail\Mail\HTMLMail',
-        'TextMail' => '\BrightNucleus\ChainMail\Mail\TextMail',
+
+    /*
+     * The `MailInterface` implementations that are provided.
+     */
+    'mails'               => [
+        'HTMLMail' => [
+            'class_name' => '\BrightNucleus\ChainMail\Mail\HTMLMail',
+        ],
+        'TextMail' => [
+            'class_name' => '\BrightNucleus\ChainMail\Mail\TextMail',
+        ],
     ],
-    'validators'       => [
-        'HTMLValidator' => '\BrightNucleus\ChainMail\Validator\HTMLValidator',
-        'TextValidator' => '\BrightNucleus\ChainMail\Validator\TextValidator',
+
+    /*
+     * The `ValidatorInterface` implementations that are provided.
+     */
+    'validators'          => [
+        'HTMLValidator' => [
+            'class_name' => '\BrightNucleus\ChainMail\Validator\HTMLValidator',
+        ],
+        'TextValidator' => [
+            'class_name' => '\BrightNucleus\ChainMail\Validator\TextValidator',
+        ],
     ],
-    'sanitizers'       => [
-        'HTMLSanitizer' => '\BrightNucleus\ChainMail\Sanitizer\HTMLSanitizer',
-        'TextSanitizer' => '\BrightNucleus\ChainMail\Sanitizer\TextSanitizer',
+
+    /*
+     * The `SanitizerInterface` implementations that are provided.
+     */
+    'sanitizers'          => [
+        'HTMLSanitizer' => [
+            'class_name' => '\BrightNucleus\ChainMail\Sanitizer\HTMLSanitizer',
+        ],
+        'TextSanitizer' => [
+            'class_name' => '\BrightNucleus\ChainMail\Sanitizer\TextSanitizer',
+        ],
     ],
-    'templates'        => [
-        'BasicTemplate'       => '\BrightNucleus\ChainMail\Template\GenericTemplate',
-        'HeroTemplate'        => '\BrightNucleus\ChainMail\Template\GenericTemplate',
-        'SidebarTemplate'     => '\BrightNucleus\ChainMail\Template\GenericTemplate',
-        'HeroSidebarTemplate' => '\BrightNucleus\ChainMail\Template\GenericTemplate',
+
+    /*
+     * The `ViewInterface` implementations that are provided.
+     */
+    'view_types'          => [
+        'PHPView' => '\BrightNucleus\ChainMail\View\PHPView',
     ],
-    'used_sections'    => [
+
+    /*
+      * The `TemplateInterface` implementations that are provided.
+      *
+      * Each template also defines what sections it intends to use, and what view
+      * it intends to use for rendering.
+      */
+    'templates'           => [
         'BasicTemplate'       => [
-            'HeaderSection',
-            'BodySection',
-            'FooterSection',
+            'class_name'    => '\BrightNucleus\ChainMail\Template\GenericTemplate',
+            'used_sections' => [
+                'HeaderSection',
+                'BodySection',
+                'FooterSection',
+            ],
+            'view_name'     => 'GenericTemplate.php',
         ],
         'HeroTemplate'        => [
-            'HeaderSection',
-            'HeroSection',
-            'BodySection',
-            'FooterSection',
+            'class_name'    => '\BrightNucleus\ChainMail\Template\GenericTemplate',
+            'used_sections' => [
+                'HeaderSection',
+                'HeroTemplate',
+                'BodySection',
+                'FooterSection',
+            ],
+            'view_name'     => 'GenericTemplate.php',
         ],
         'SidebarTemplate'     => [
-            'HeaderSection',
-            'BodySection',
-            'SidebarSection',
-            'FooterSection',
+            'class_name'    => '\BrightNucleus\ChainMail\Template\GenericTemplate',
+            'used_sections' => [
+                'HeaderSection',
+                'BodySection',
+                'SidebarTemplate',
+                'FooterSection',
+            ],
+            'view_name'     => 'GenericTemplate.php',
         ],
         'HeroSidebarTemplate' => [
-            'HeaderSection',
-            'HeroSection',
-            'BodySection',
-            'SidebarSection',
-            'FooterSection',
+            'class_name'    => '\BrightNucleus\ChainMail\Template\GenericTemplate',
+            'used_sections' => [
+                'HeaderSection',
+                'HeroTemplate',
+                'BodySection',
+                'SidebarTemplate',
+                'FooterSection',
+            ],
+            'view_name'     => 'GenericTemplate.php',
         ],
     ],
-    'sections'         => [
-        'HeaderSection'  => '\BrightNucleus\ChainMail\Section\GenericSection',
-        'BodySection'    => '\BrightNucleus\ChainMail\Section\GenericSection',
-        'FooterSection'  => '\BrightNucleus\ChainMail\Section\GenericSection',
-        'HeroSection'    => '\BrightNucleus\ChainMail\Section\GenericSection',
-        'SidebarSection' => '\BrightNucleus\ChainMail\Section\GenericSection',
-    ],
-    'view_types'       => [
-        'PHPView' => '\BrightNucleus\ChainMail\View\PHPView',
+
+    /*
+     * The `SectionInterface` implementations that are provided.
+     *
+     * Each section also defines what view it intends to use for rendering.
+     */
+    'sections'            => [
+        'HeaderSection'  => [
+            'class_name' => '\BrightNucleus\ChainMail\Section\GenericSection',
+            'view_name'  => 'GenericSection',
+        ],
+        'BodySection'    => [
+            'class_name' => '\BrightNucleus\ChainMail\Section\GenericSection',
+            'view_name'  => 'GenericSection',
+        ],
+        'FooterSection'  => [
+            'class_name' => '\BrightNucleus\ChainMail\Section\GenericSection',
+            'view_name'  => 'GenericSection',
+        ],
+        'HeroSection'    => [
+            'class_name' => '\BrightNucleus\ChainMail\Section\GenericSection',
+            'view_name'  => 'GenericSection',
+        ],
+        'SidebarSection' => [
+            'class_name' => '\BrightNucleus\ChainMail\Section\GenericSection',
+            'view_name'  => 'GenericSection',
+        ],
     ],
 ];
