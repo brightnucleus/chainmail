f961de0947fbc61c8b89a5149d3d91648593913f
Alain Schlesser <alain.schlesser@gmail.com>
2016-05-27 16:27:33 +0200

    Change license from GPL-v2.0+ to MIT.

--- config/defaults.php
+++ config/defaults.php
@@ -7,7 +7,7 @@
  *
  * @package   brightnucleus/chainmail
  * @author    Alain Schlesser <alain.schlesser@gmail.com>
- * @license   GPL-2.0+
+ * @license   MIT
  * @link      http://www.brightnucleus.com/
  * @copyright 2016 Alain Schlesser, Bright Nucleus
  */
